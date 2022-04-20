f6dbcdaf63396f8f636120cf91f427e22cb082e1
Cyrille Umehara <Cyrilleh@live.fr>
2022-04-20 12:24:42 +0200

    Clear db & add css

--- migrations/Version20220420102305.php
+++ migrations/Version20220420102305.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220420093745 extends AbstractMigration
+final class Version20220420102305 extends AbstractMigration
 {
     public function getDescription(): string
     {
